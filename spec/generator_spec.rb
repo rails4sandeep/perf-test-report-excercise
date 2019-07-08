b5ba497cd3ae2ff6c37e0a9175094c3308cb6db9
sandeepkumar <sandeep.kumar@flamingo.ai>
2019-07-08 14:29:54 +1000

    Add tests related to the generator class

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,18 +1,10 @@
 require 'spec_helper'
 
-RSpec.describe "Performance test report generator" do
+RSpec.describe "Generator Spec" do
 
-  it 'should generate a summary report' do
-=begin
-    @extractor = Extractor.new
-    report_data_set = @extractor.build_data_set('./spec/reports')
-
-    @organizer = Organizer.new(report_data_set)
-    reports_hash = @organizer.create_reports_hash
-    @organizer.generate_reports_json(reports_hash)
-  end
-=end
-  @generator = Generator.new('./spec/reports')
-  @generator.generate_json_report
+  it 'generate_json_report should generate a summary report' do
+    @generator = Generator.new('./spec/reports')
+    @generator.generate_json_report
+    expect(File.extname('../public/temp.json')).to eql('.json')
   end
 end
